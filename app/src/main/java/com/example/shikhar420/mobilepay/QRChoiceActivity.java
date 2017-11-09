e56346fe69c9169559104439fae1e3fbee51d1b0
Rishabh Kumar Jha <rishabhkumarjha@gmail.com>
2017-11-09 18:11:51 +0000

    QR Reciever And sender Added

--- app/src/main/java/com/example/shikhar420/mobilepay/QRChoiceActivity.java
+++ app/src/main/java/com/example/shikhar420/mobilepay/QRChoiceActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+
 public class QRChoiceActivity extends AppCompatActivity {
 
     @Override
@@ -18,5 +20,9 @@ public class QRChoiceActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(getApplicationContext(), QRScannerActivity.class);
         startActivity(intent);
+    }
+
+    public void generateQRonclick(View view){
+        new IntentIntegrator(this).shareText("1234567890");
     }
 }
